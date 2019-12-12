30e7e9aeef502b83cbc40d74d5194a1ade0e20ed
moogiecodes <mchoichoi@gmail.com>
2019-12-11 23:35:11 -0800

    Refactor code, add matchInProg state to prevent fast clicking bug

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ let cardDivs = document.getElementsByClassName("card-div");
 let cardDivsArray = [...cardDivs];
 let card1, card2;
 let flippedState = false;
+let matchInProg = false;
 
 function gameStart() {
   // event listeners for all cards
-  for (let i = 0; i < cardDivsArray.length; i++) {
-    cardDivsArray[i].addEventListener("click", flipCard);
-    cardDivsArray[i].addEventListener("click", flipCardDisplay);
-  }
+  cardDivsArray.forEach(card => {
+    card.addEventListener("click", flipCard);
+    card.addEventListener("click", flipCardDisplay);
+  });
 }
 function flipCardDisplay() {
   document.getElementById("flippedCounter").innerText =
@@ -17,46 +18,63 @@ function flipCardDisplay() {
 }
 // when a card is clicked
 function flipCard() {
-  this.classList.toggle("disableClick"); // disables clicking on same card
-  this.classList.toggle("visible"); // reveals card
-  selectedCards++; // increment total selected cards counter
-  // store card 1 & card 2
+  if (matchInProg === true) return; // do nothing if there is a match in progress
+  this.classList.add("disableClick"); // disables clicking on same card
+  if (matchInProg === false) {
+    // if not currently checking for a match
+    this.classList.add("visible"); // reveals card
+    selectedCards++; // increment total selected cards counter
+  }
+  // store card 1 and card 2 vars
   if (flippedState === false) {
     card1 = this;
     flippedState = true;
+    return;
   } else {
     flippedState = false;
     card2 = this;
   }
-  console.log("card1 is" + card1);
-  console.log("card2 is" + card2);
-  // if cards match
-  if (card1 !== undefined && card2 !== undefined) {
+  // initiate match testing
+  checkCardPair();
+}
+function checkCardPair() {
+  // check if cards hold value
+  if (card1 !== null && card2 !== null) {
+    // if both cards are a MATCH
     if (
       card1.getElementsByClassName("backface-img")[0].src ===
       card2.getElementsByClassName("backface-img")[0].src
     ) {
-      console.log("it's a match!");
+      // remove event listeners on just those 2 cards
+      card1.removeEventListener("click", flipCard);
+      card1.removeEventListener("click", flipCardDisplay);
+      card2.removeEventListener("click", flipCard);
+      card2.removeEventListener("click", flipCardDisplay);
+      // add "matched" class
+      card1.classList.add("match");
+      card2.classList.add("match");
+    }
+    // if card DOESN'T match
+    else {
+      matchInProg = true;
+      //after 1 sec, flips back, re-enable click
+      setTimeout(() => {
+        card1.classList.remove("visible", "disableClick");
+        card2.classList.remove("visible", "disableClick");
+        // change checking state back to false, so card can be visible again
+        matchInProg = false;
+      }, 1000);
     }
   }
-  // if card doesn't match
-  if (
-    card1.getElementsByClassName("backface-img")[0].src !==
-    card2.getElementsByClassName("backface-img")[0].src
-  ) {
-    //after 2 secs, flips back, re-enable click
-    setTimeout(() => {
-      card1.classList.remove("visible", "disableClick");
-      card2.classList.remove("visible", "disableClick");
-    }, 2000);
-  }
 }
-/* Notes: 
+
+/* 
+Notes: 
 - selectedCards is even #, initiate function to compare match ?
 - disable click on rest of the cards when comparing 2 cards ? or until both cards flipped back
 
 - make an array to hold matched cards?
-- if array reaches length of cards array, victory?
+- if array reaches length of matched array or count, victory? document.getElementsByClassName("match").length
 - function that sets random pics under cards on START GAME button click?
 */
 
